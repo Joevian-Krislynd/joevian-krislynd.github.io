b8b93733150425fb4984f1f403c807b8f1c602b5
Joevian Krislynd <62868572+JoeKrislynd@users.noreply.github.com>
2026-07-07 15:01:23 +0700

    Initial Commit

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import injectHTML from 'vite-plugin-html-inject'
+import { resolve } from 'path'
+
+export default defineConfig({
+    root: resolve(__dirname, 'src'),
+
+    resolve: {
+        alias: {
+            '~bootstrap': resolve(__dirname, 'node_modules/bootstrap'),
+        }
+    },
+
+    server: {
+        port: 8080,
+        open: true
+    },
+
+    build: {
+        outDir: '../dist',
+        emptyOutDir: true
+    },
+
+    plugins: [injectHTML()]
+})
